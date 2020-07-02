306ef32db1c7fe15dacddf6bee5bdbb6252d07ca
Julius-r <julius.robinson95@yahoo.com>
2020-07-02 12:47:32 -0500

    Added Logger UI components

--- src/Components/Log.js
+++ src/Components/Log.js
@@ -8,7 +8,7 @@ export default function Log() {
 				User states that weird message appears on monitor and does not
 				disappear
 			</td>
-			<td className="border px-4 py-2">Medium</td>
+			<td className="border px-4 py-2 text-yellow-500">Medium</td>
 			<td className="border px-4 py-2">06/23/2020 @ 12:32PM</td>
 			<td className="border px-4 py-2">Edit...</td>
 			<td className="border px-4 py-2">Delete...</td>
